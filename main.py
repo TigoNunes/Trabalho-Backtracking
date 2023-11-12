52406645fbd3857b28bd9c46627f32609aa1a572
Tiago Nunes de Cerqueira <83711090+TigoNunes@users.noreply.github.com>
2023-11-12 16:06:31 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import itertools
 from menu import Menu
 menu = Menu()
 teste = menu.teste
@@ -5,33 +6,59 @@ k = menu.k
 
 # Força Bruta#####################################################
 def forca_bruta(teste, k):
-    # TODO: Colocar marcadores de tempo
-    for i in range(len(teste) - 1):
-        for j in range(i+1, len(teste)):
-            save = j
-            res = teste[i] + teste[j]
-            sub_teste = [teste[i], teste[j]]
-            while res < k:
-                j += 1
-                try:
-                    res += teste[j]
-                    sub_teste.append(teste[j])
-                except IndexError:
-                    res = 10000
-                
+    conjuntos_de_subconjuntos = []
+    for i in range(2, len(teste)-1):
+        subconjuntos = list(itertools.combinations(teste, i))
+        conjuntos_de_subconjuntos.append(subconjuntos[:])
+    
+    respostas = []
+    for conjunto in conjuntos_de_subconjuntos:
+        for sub in conjunto:
+            res = sum(sub)
+            if res == k:
+                respostas.append(sub)                
 
-            if res > k:
-                print(f"falha\t{sub_teste}")
-            else:
-                print(f"ACHADO\t{sub_teste}")
-                
-            j = save
+    return respostas
+
 
 # Backtracking####################################################
 
-def backtrackin(teste,k):
-    # TODO: Fazer o código 
-    # TODO: Marcadores de tempo
-    pass
+# def backtracking(teste,k, resultados:list, subconjuntos:list):
+    
+#     if len(teste) > 0:
+#         if k - teste[0] > 0:
+#             subconjuntos.append(teste[0])            
+#             resultados.append(backtrackin(teste[1:], k - teste[0], resultados,subconjuntos))
 
-forca_bruta(teste,k)
+#             # subconjuntos.pop(teste[0])
+#             resultados.append(backtrackin(teste[1:], k, resultados, subconjuntos))
+
+#         elif k - teste[0] < 0: # Se a conta der menos que k, algoritimo retorna
+#             resultados.append(backtrackin(teste[1:], k, resultados, subconjuntos))
+#         else: return subconjuntos.append(teste[0])
+#     elif k > 0:
+#         return []
+#     else: return subconjuntos
+
+#     # return resultados
+
+def backtracking(numbers, target_sum):
+    def backtrack(start, current_sum, current_subset):
+        if current_sum == target_sum:
+            subsets.append(current_subset[:])
+            return
+        if current_sum > target_sum or start == len(numbers):
+            return
+        for i in range(start, len(numbers)):
+            current_subset.append(numbers[i])
+            backtrack(i + 1, current_sum + numbers[i], current_subset)
+            current_subset.pop()
+
+    subsets = []
+    backtrack(0, 0, [])
+    return subsets
+             
+print(f"{forca_bruta(teste,k)}\n")
+
+print(backtracking(teste, k))
+
